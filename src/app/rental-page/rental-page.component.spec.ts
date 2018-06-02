cc548c4495529eff5025bdb7354eae60fa2e7e40
term96 <term96@yandex.com>
2018-06-02 19:09:36 +0300

    Реализована основная часть формы

---
+++ src/app/rental-page/rental-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RentalPageComponent } from './rental-page.component';
+
+describe('RentalPageComponent', () => {
+  let component: RentalPageComponent;
+  let fixture: ComponentFixture<RentalPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RentalPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RentalPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
